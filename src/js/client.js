9d2eea8d8497d41a916e76eef172bc0237a8df5c
Oskar <oskarradonkimball@gmail.com>
2016-08-11 18:19:29 -0700

    Refactor Layout into a component

--- src/js/client.js
+++ src/js/client.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-
-	howImFeeling() {
-		return "happy";
-	}
-
-	render() {
-		return (
-			<h1>I'm feeling {this.howImFeeling()}</h1>
-		)
-	}
-}
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 
