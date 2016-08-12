619a69b37ac958d9aedb7da532670d25d0538164
Oskar <oskarradonkimball@gmail.com>
2016-08-11 18:34:03 -0700

    Remove function from lower component

--- src/js/components/Feelings.js
+++ src/js/components/Feelings.js
@@ -2,13 +2,9 @@ import React from "react";
 
 
 export default class Feelings extends React.Component {
-	howImFeeling() {
-		return "happy";
-	}
-
 	render() {
 		return (
-			<h1>I'm feeling {this.howImFeeling()}</h1>
+			<h1>I'm feeling gooood</h1>
 		)
 	}
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -5,17 +5,8 @@ import Feelings from "./Feelings"
 
 export default class Layout extends React.Component {
 	render() {
-
-	let feelingsList = [
-		<Feelings />,
-		<Feelings />,
-		<Feelings />
-	]
-
 		return (
-			<ul>
-				{feelingsList}
-			</ul>
+			<Feelings />
 		)
 	}
 }
