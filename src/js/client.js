9e7c0b7ffd62b6be09944154f4b84ba2bcea45ed
Oskar <oskarradonkimball@gmail.com>
2016-08-16 09:44:07 -0700

    Link to Container

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./pages/Layout"
+import Container from "./pages/Container"
 import Blog from "./pages/Blog"
 import About from "./pages/About"
 
@@ -14,7 +14,7 @@ const NotFound = () => (
 
 ReactDOM.render(
 	<Router history={hashHistory}> 
-		<Route path="/" component={Layout}>
+		<Route path="/" component={Container}>
 			<IndexRoute component={Blog}></IndexRoute>
 			<Route path="about" component={About}></Route>
 			<Route path='*' component={NotFound} />
