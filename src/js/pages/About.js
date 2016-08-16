b7639d32d9c186e12b348103532f4d767cc17554
Oskar <oskarradonkimball@gmail.com>
2016-08-16 09:44:32 -0700

    About route now has Emotion component

--- src/js/pages/About.js
+++ src/js/pages/About.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Feelings from "../components/Feelings"
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			emotion: "good",
+		};
+	}
+	moodSwing(emotion) {
+		this.setState({emotion});
+	}
+	
 	render() {
 		return (
 			<main>
-				<h1>All about me :)</h1>
+				<h1>Hey I'm Oskar</h1>
+				<Feelings moodSwing={this.moodSwing.bind(this)} emotion={this.state.emotion}/>
 			</main>
 		)
 	}
