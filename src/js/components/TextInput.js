2f3d7af82e9041e8601c66f4e6f1ebfba9622f47
Oskar <oskarradonkimball@gmail.com>
2016-08-21 22:07:18 -0700

    Add TextInput component and implement

---
+++ src/js/components/TextInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TextInput extends React.Component {
+	render() {
+		return (
+			<div>
+				<input />
+				<button>Add</button>
+			</div>
+		);
+	}
+}
--- src/js/pages/ToDo.js
+++ src/js/pages/ToDo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import TextInput from '../components/TextInput';
+
 export default class Layout extends React.Component {
 	render() {
 		return (
 			<main>
 				<h1>To Do List:</h1>
+				<TextInput />
 			</main>
 		);
 	}
